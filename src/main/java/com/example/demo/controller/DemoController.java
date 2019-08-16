78cf255047e0236ed387d93e9fd059c3adbcecdc
chengang <chengang@vcredit.com>
2019-08-16 17:15:27 +0800

    demo初始化提交

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: chengang
+ * @date: 2019/8/16
+ * @description:
+ */
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello 华为云！";
+    }
+}
